75a94205fee5e64c9c01108461687088fc4ab8ce
SavoCv <savo.cv2002@gmail.com>
2023-12-15 21:14:28 +0100

    Merged

--- Taxi/NISTaxi/urls.py
+++ Taxi/NISTaxi/urls.py
@@ -1,16 +1,7 @@
-<<<<<<< HEAD
 from django.urls import path
 from .views import *
 
 urlpatterns = [
     path('', ping),
     path('login', login_req),
-=======
-from django.contrib import admin
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('',index,name='index')
->>>>>>> 4736802c0857e131c0fc329d18f8904b4accc328
 ]
