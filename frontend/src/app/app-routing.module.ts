a1ef5014452b47428828ecba44e027678abeec07
Petar Vasiljevic <petarvasiljevic2001@gmail.com>
2023-12-16 10:39:50 +0100

    fixed bug

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,11 +4,12 @@ import { LoginComponent } from './login/login.component';
 import { RecommendComponent } from './recommend/recommend.component';
 import { HomeComponent } from './home/home.component';
 import { LogoutComponent } from './logout/logout.component';
+import { LoginReferralComponent } from './login-referral/login-referral.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-  // { path: 'login/:ref_code', component: LoginReferralComponent },
+  { path: 'login/:ref_code', component: LoginReferralComponent },
   { path: 'recommend', component: RecommendComponent },
   { path: 'home', component: HomeComponent },
   { path: 'logout', component: LogoutComponent },
